ef03777394f4ed72358d3d739e2f73c15abd4eff
Pacidus <pacidus@gmail.com>
2022-06-13 10:57:10 +0200

    Progress on Tools.

--- DATA/Tools.py
+++ DATA/Tools.py
@@ -1,6 +1,51 @@
-################
-#   Imports    #
-# Nothing much #
-################
+######################
+# --    Imports   -- #
+# -- Nothing much -- #
+######################
 import pandas as pd
 import pyarrow.parquet as pq
+
+
+#########################
+# -- Global Varibles -- #
+# --      AKA        -- #
+# --    Aliases      -- #
+# Statics variables ofc #
+#########################
+
+dsmpl = int(1e6)
+didx = None
+dkwa = dict()
+darg = []
+
+
+def aparquet(pf, func, args=darg, kwargs=dkwa, index=didx, sample=dsmpl):
+    """
+    apply a function on a parquet file by sample.
+
+    Parameters
+    ----------
+    pf : pq.ParquetFile
+        the parquet file we want to itterate through.
+    func : python function
+        funtion that take a panda dataframe in input and *args, **kwargs.
+    args : list or tuple
+        arguments of func.
+    kwargs : dict
+        keyword arguments of func
+    index : list(str)
+        index we want to keep from pf
+    sample : int
+        size of the sample
+
+    Returns
+    -------
+    list
+        list of the outputs of func(dtf, *args, **kwargs)
+    """
+    batches = pf.iter_batches(sample, columns=index)
+    out = []
+    for batch in batches:
+        data = batch.to_pandas()
+        out.append(func(data, *args, **kwargs))
+    return out
