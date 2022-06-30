5d469f280982906ed9b645b31986410f854eb3e6
Pacidus <pacidus@gmail.com>
2022-06-30 15:58:37 +0200

    Update develop

--- test/io_test.py
+++ test/io_test.py
@@ -11,6 +11,10 @@ def is_like(v, vtrue, rdiff=1e-5):
     return np.mean(np.abs((v - vtrue) ** 2 / vtrue)) < rdiff
 
 
+def absinf(df):
+    return np.abs(df["Int2e"]) < 1e-3
+
+
 def test_convert_file():
     Tools.csv2parquet("test/n_test.parquet", "test/test.csv")
     assert path.exists("test/n_test.parquet")
@@ -29,3 +33,22 @@ def test_stats():
     assert stats_itt.N == stats_dir.N
     assert is_like(stats_itt.var, stats_dir.var, 1e-5)
     assert is_like(stats_itt.mean, stats_dir.mean, 1e-20)
+
+
+def test_split():
+    pf = Tools.pq.ParquetFile("test/test.parquet")
+
+    Tools.sparq(pf, absinf, name="test/out")
+
+    pff = Tools.pq.ParquetFile("test/out_False.parquet")
+    pft = Tools.pq.ParquetFile("test/out_True.parquet")
+
+    stats = Tools.stats(pf)
+    statst = Tools.stats(pft)
+    statsf = Tools.stats(pff)
+
+    assert stats.N == statst.N + statsf.N
+    assert np.abs(statst.min["Int2e"]) < 1e-3
+    assert np.abs(statst.max["Int2e"]) < 1e-3
+    remove("test/out_False.parquet")
+    remove("test/out_True.parquet")
